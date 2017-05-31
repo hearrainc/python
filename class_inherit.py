86f203cc59b91b5b952a82b198a35e066bde7bab
chenlin <chen.lin322@zte.com.cn>
2017-05-31 19:24:48 -0400

    add br base-content

--- class_inherit.py
+++ class_inherit.py
@@ -2,17 +2,17 @@
 #coding:utf-8
 
 class SchoolMember: 
-'''Represents any school member.''' 
+    '''Represents any school member.''' 
     def __init__(self, name, age): 
         self.name = name 
         self.age = age 
         print '(Initialized SchoolMember: %s)' % self.name
     def tell(self): 
-    '''Tell my details.''' 
+        '''Tell my details.''' 
         print 'Name:"%s" Age:"%s"' % (self.name, self.age), 
 
 class Teacher(SchoolMember): 
-'''Represents a teacher.''' 
+    '''Represents a teacher.''' 
     def __init__(self, name, age, salary): 
         SchoolMember.__init__(self, name, age) 
         self.salary = salary 
@@ -22,7 +22,7 @@ class Teacher(SchoolMember):
         print 'Salary: "%d"' % self.salary
 
 class Student(SchoolMember): 
-'''Represents a student.''' 
+    '''Represents a student.''' 
     def __init__(self, name, age, marks): 
         SchoolMember.__init__(self, name, age) 
         self.marks = marks 
