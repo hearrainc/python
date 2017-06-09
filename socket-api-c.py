ee272e7e172293772d4306a6fe6987ea2ed89420
chenlin <chen.lin322@zte.com.cn>
2017-06-08 20:44:37 -0400

    api test

---
+++ socket-api-c.py
@@ -0,0 +1,23 @@
+﻿#!/usr/bin/python
+#-*- coding: utf-8 -*-
+
+import socket
+
+try:
+    HOST='127.0.0.1'
+    PORT=50007
+    s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    s.connect((HOST,PORT))
+except Exception as e:
+    print str(e)
+    
+while 1:
+    try:
+        cmd=raw_input("Please input>")
+        s.sendall(cmd)
+        data=s.recv(1024)
+        print data
+    except KeyboardInterrupt,e:
+        print str(e)
+        break
+s.close()
---
+++ socket-api-s.py
@@ -0,0 +1,39 @@
+﻿#!/usr/bin/python
+#-*- coding: utf-8 -*-
+
+import sys
+import socket
+
+try:
+    HOST=''
+    PORT=50007
+    s= socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    s.bind((HOST,PORT))
+    s.listen(1)
+except Exception as e:
+    print str(e)
+    sys.exit(0)
+
+print 'start listen'
+
+client = None
+while 1:
+    try:
+        client,addr=s.accept()
+        print 'Connected by',addr
+        client.settimeout(6)
+        while 1:
+                data=client.recv(1024)
+                if not data:
+                    client.close()
+                    print 'empty'
+                    break
+                print 'c say',data
+                recv = 's say ' + data
+                client.sendall(recv)
+    except Exception as e:
+        print str(e)
+        break
+if client is not None:
+    client.close()
+s.close()
